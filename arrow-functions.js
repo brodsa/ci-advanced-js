2a195686314336747df516212f3136a093aabce7
brodsa <brodinova@gmail.com>
2023-07-31 22:08:11 +0000

    feat: explain the concept of arrow functions on small examples

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,40 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumber(a, b){
+//     // code block
+//     return a + b;
+// }
+// let sum = addTwoNumber(3,5)
+// console.log(sum)
+
 
 // Arrow Function With Parameters
+// const addTwoNumbers = (a, b) => {
+//     // code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3,5)
+// console.log(sum)
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumber = (a, b) => a + b;
+// const addTwoNumber = (a, b) => (a + b);
+// let sum = addTwoNumber(3,5)
+// console.log(sum)
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('hello there');
+
+const sayHello = () => console.log('Hello')
 
 // Returning Multiple Lines
+const returnMultipleLines = () => {
+    `<p>
+     This is a multiple line string
+    </p> 
+    `
+}
+console.log(returnMultipleLines)
+
